e5ad536515ce038a9d24f9c65a78ca564ea3535c
chrisjal <cjalallian@gmail.com>
2017-07-08 22:42:19 -0700

    re-worked JS into jQuery, changed a few elements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,78 +1,59 @@
-// created: July 2, 2017 12:00PM
+// created: July 8, 2017 18:00
 // Author: Chris Jalallian
-// Revisions: CJ 7/2 16:30 finished game logic, commented code
+// Revisions: 
 
-var goalPoints, redValue, blueValue, greenValue, purpleValue;
+var goalPoints, redValue, blueValue, greenValue, purpleValue, playerPoints = 0;
+
+// Set to begin @1 because first increment would be 0 otherwise, as it is not written elsewhere
 var wins = 1;
 var losses = 1;
 
-
-// Instructions
+// Instruction toggle
 $("#instruction-toggle").click(function(){
 	$("#instruction-container").slideToggle("slow");
 });
 
 function game() {
-	//resets playerPoints to 0
+	// Resets playerPoints to 0
 	playerPoints = 0;
-	document.getElementById("currentPoints").innerHTML = playerPoints = 0;
-	
-	//Generates Goal # between 19-120
+	$("#current-points").html(playerPoints);
+
+	// Generates Goal # between 19-120
 	goalPoints = [Math.floor(Math.random() * (120 - 19 + 1)) + 19];
-	document.getElementById("goalValue").innerHTML = goalPoints;
+	$("#goalValue").html(goalPoints);
 	
-	//Generate random values for the Jewels
+	// Generate random values for the Jewels 1-12
 	redValue = [Math.floor(Math.random() * (12 - 1 + 1)) + 1];
 	blueValue = [Math.floor(Math.random() * (12 - 1 + 1)) + 1];
 	greenValue = [Math.floor(Math.random() * (12 - 1 + 1)) + 1];
 	purpleValue = [Math.floor(Math.random() * (12 - 1 + 1)) + 1];
-
+	
+	// Set html element values
+	$("#redButton").val(redValue);
+	$("#blueButton").val(blueValue);
+	$("#greenButton").val(greenValue);
+	$("#purpleButton").val(purpleValue);
 }
 
 function checkwin() {
-	//if loss, restart game, write loss +1
+	// If win/loss, restart game, write incremental result
 	if (playerPoints > goalPoints) {
 		game();
-		return document.getElementById("lossTotal").innerHTML = losses++;
+		$("#lossTotal").html(losses++).effect("bounce","slow");
 	}
-	//if win, restart game, write win +1
 	if (playerPoints == goalPoints) {
 		game();
-		return document.getElementById("winTotal").innerHTML = wins++;
+		$("#winTotal").html(wins++).effect("bounce","slow");
 	}
 }
 
-// Incrementing jewel values below add to playerPoints
-function red() {
-	playerPoints = playerPoints + parseInt(redValue);
-	document.getElementById("currentPoints").innerHTML = playerPoints;
-}
-
-function blue() {
-	playerPoints = playerPoints + parseInt(blueValue);
-	document.getElementById("currentPoints").innerHTML = playerPoints;
-}
-
-function green() {
-	playerPoints = playerPoints + parseInt(greenValue);
-	document.getElementById("currentPoints").innerHTML = playerPoints;
-}
-
-function purple() {
-	playerPoints = playerPoints + parseInt(purpleValue);
-	document.getElementById("currentPoints").innerHTML = playerPoints;
-}
-
-function addValue(value) {
-	playerPoints = playerPoints + parseInt(value);
-	document.getElementById("currentPoints").innerHTML = playerPoints;
-}
-// $('#redButton').click(function() {
-// 	red();
-// 	checkwin();
-// })
-// $('button').click(function() {
-// 	var value = window[$(this).attr('data-value')]
-// })
+// Click button gets jewel value and adds to current points, runs effects, and checks win condition
+$('button').click(function() {
+	var buttonValue = $(this).attr('value');
+	playerPoints = playerPoints + parseInt(buttonValue);
+	$("#current-points").html(playerPoints).effect("highlight","fast");
+	$("#last-points").html("+" + buttonValue).fadeIn().fadeOut();
+	checkwin();
+})
 
 game();
